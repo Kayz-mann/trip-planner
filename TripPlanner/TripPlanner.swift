f06e26619eaf6d18b3aa2101d7b90f47dde8b385
Kayz-mann <kayodefredrickbalogun@gmail.com>
2025-12-12 23:22:31 +0100

    Initial Commit

---
+++ TripPlanner/TripPlanner.swift
@@ -0,0 +1,9 @@
+//
+//  TripPlanner.swift
+//  TripPlanner
+//
+//  Created by Balogun Kayode on 12/12/2025.
+//
+
+import Foundation
+
---
+++ TripPlannerTests/TripPlannerTests.swift
@@ -0,0 +1,17 @@
+//
+//  TripPlannerTests.swift
+//  TripPlannerTests
+//
+//  Created by Balogun Kayode on 12/12/2025.
+//
+
+import Testing
+@testable import TripPlanner
+
+struct TripPlannerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
